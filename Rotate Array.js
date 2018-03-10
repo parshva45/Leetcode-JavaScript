edc0a86744a2e5bc30a1a358774635097a0be053
parshva45 <parshva45@gmail.com>
2018-03-10 18:57:38 -0500

    Rotate Array

---
+++ Rotate Array.js
@@ -0,0 +1,16 @@
+/*Rotate an array of n elements to the right by k steps.
+
+For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    k = k % nums.length
+    for(i=0; i<k; i++){
+        last = nums.pop(nums.length-1)
+        nums.unshift(last)
+    }
+};

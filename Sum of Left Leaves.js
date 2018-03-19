905b2c3332bcb8ca57ba23eb627b57d87b3b56e9
parshva45 <parshva45@gmail.com>
2018-03-19 16:18:25 -0400

    Sum of Left Leaves

---
+++ Sum of Left Leaves.js
@@ -0,0 +1,36 @@
+/*Find the sum of all left leaves in a given binary tree.
+
+Example:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumOfLeftLeaves = function(root) {
+    if(root == null)
+        return 0
+    if(isLeaf(root.left))
+        return root.left.val + sumOfLeftLeaves(root.right)
+    return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right)
+};
+
+var isLeaf = function(node){
+    if(node == null || node.left != null || node.right != null)
+        return false
+    return true
+};

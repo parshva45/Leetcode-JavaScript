d25104f50427f2d75831858ca540f48021a641a7
parshva45 <parshva45@gmail.com>
2018-03-21 20:02:21 -0400

    Number of Segments in a String

---
+++ Number of Segments in a String.js
@@ -0,0 +1,21 @@
+/*Count the number of segments in a string, where a segment is defined to be a contiguous sequence of non-space characters.
+
+Please note that the string does not contain any non-printable characters.
+
+Example:
+
+Input: "Hello, my name is John"
+Output: 5*/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSegments = function(s) {
+    arr = s.split(' ')
+    count = 0
+    for(i=0; i<arr.length; i++)
+        if(arr[i] != '')
+            count++
+    return count
+};
